d8f32ce5a4d027eb9df0935355530ff29104426a
ryanshu <876324872@qq.com>
2021-05-30 01:08:05 +0800

    config:rename file

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,9 @@ from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
-from anonymous_mood import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include("mood.urls")),
-    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path("admin/", admin.site.urls),
+    path("api/", include("mood.urls")),
+    re_path(r"media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
